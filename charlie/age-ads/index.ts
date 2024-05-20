121c910dcc37a568fcfd6bd1fc7ce59d35a010a7
Vincent Trivett <trivett@gmail.com>
2024-05-20 09:13:43 -0400

    fixed typing

--- charlie/age-ads/index.ts
+++ charlie/age-ads/index.ts
@@ -13,35 +13,24 @@ const domainKeys = (fullDomain: string): string[] => {
 
 export const calculateClicksByDomain = (
   input: string[]
-): { [key: string]: string } => {
-  const tally = input.reduce((acc, line) => {
+): { [key: string]: number } => {
+  const tally = input.reduce((acc: { [key: string]: number }, line: string) => {
     const splitLine = line.split(",");
     const clicks = Number(splitLine[0]);
     let domainFull = splitLine[1];
 
     const allDomains = domainKeys(domainFull);
 
-    console.log(allDomains);
-
     allDomains.forEach((item) => {
       if (acc[item]) {
         acc[item] = acc[item] + clicks;
-        // return;
       } else {
         acc[item] = clicks;
       }
     });
 
-    // split full domain into its pieces and loop
-    // increment value in object
-
     return acc;
   }, {});
-
-  // const sumWithInitial = array1.reduce(
-  //   accumulator + currentValue,
-  //   initialValue,
-  // );
 
   return tally;
 };
